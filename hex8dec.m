053f3131321353fe2b467dcbe29d9e5cc2997801
hht238000 <62174304+hht238000@users.noreply.github.com>
2020-04-04 18:09:35 +0800

    Add files via upload

---
+++ hex8dec.m
@@ -0,0 +1,30 @@
+function f=hex8dec(mat)
+format long g
+integer=mat(4:7);
+decimal=mat(8:11);
+
+int=0;n=length(integer);
+
+for i=1:n
+
+    int=hex2dec(integer);
+
+end
+
+dec=0;m=length(decimal);
+
+for i=1:m
+
+    dec=dec+hex2dec(decimal(i))*16^(-i);
+
+end
+
+f=int+dec;
+mat(3);
+if hex2dec(mat(4))>=8
+    f=f-8*16^3;
+    f=-f;
+else f=f;
+end
+
+%8''h80018000
