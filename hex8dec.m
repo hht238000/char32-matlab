97e83036bf72fbd5898f50a1210ed59aac62f416
hht238000 <62174304+hht238000@users.noreply.github.com>
2021-04-16 22:00:35 +0800

    Update hex8dec.m
    原本的b结构使用有bug，做了修复

--- hex8dec.m
+++ hex8dec.m
@@ -1,3 +1,4 @@
+% ----------------a--------------------
 function f=hex8dec(mat)
 format long g
 integer=mat(4:7);
@@ -27,7 +28,24 @@ if hex2dec(mat(4))>=8
 else f=f;
 end
 
+
+
+% --------------------b------------------------
+function f=hex32dec_complement(mat)
+
+
+TwsComp = hex2dec(mat);
+
+
+threshold = hex2dec('80000000');
+if(TwsComp>threshold)
+    f = (TwsComp - (hex2dec('FFFFFFFF')+1))/2^(16);
+else
+    f = TwsComp/2^(16);
+
+
+
+end
+
 %'8''h80018000'
-%为了补码形式下的同格式数值进制转换，提供以下语句，语句含示例
-%In order to convert the same format numerical system in the complement form, the following sentence is provided, and the sentence contains an example
-%double(typecast(uint32(hex2dec('ffff0000')), 'int32'))/(2^(16))
+
